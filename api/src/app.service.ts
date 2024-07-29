2bd1d1bd82187430fb3373d6932ecf1b74e4846d
adriansajjan <adriansajjan2001@gmail.com>
2024-07-29 21:16:30 +0530

    minor changes

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 import * as fs from 'fs';
 
 import { Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { from, Observable } from 'rxjs';
 import { ElevenLabsClient } from 'elevenlabs';
 
 @Injectable()
@@ -12,16 +12,17 @@ export class AppService {
 
   createAudioFileFromText(text: string) {
     return new Observable<string>((subscriber) => {
-      const id = uuid.v4();
-      const file = `${id}.mp3`;
-      const name = path.join(__dirname, '..', 'uploads', 'audio', file);
-      (async () => {
-        try {
-          const audio = await this.elevanlabsClient.generate({
-            voice: 'Rachel',
-            model_id: 'eleven_turbo_v2_5',
-            text,
-          });
+      from(
+        this.elevanlabsClient.generate({
+          voice: 'Rachel',
+          model_id: 'eleven_turbo_v2_5',
+          text,
+        }),
+      ).subscribe({
+        next(audio) {
+          const file = `${uuid.v4()}.mp3`;
+          const name = path.join(__dirname, '..', 'uploads', 'audio', file);
+
           const buffer: Uint8Array[] = [];
           const stream = new WritableStream({
             write(chunk) {
@@ -30,20 +31,22 @@ export class AppService {
             close() {
               const data = Buffer.concat(buffer);
               fs.writeFileSync(name, data);
-              subscriber.next(name);
+              subscriber.next(name.split('uploads').pop());
               subscriber.complete();
             },
             abort() {
               subscriber.error();
             },
           });
+
           fs.createWriteStream(name);
           // @ts-expect-error To be fixed in elevenlabs SDK
           audio.pipeTo(stream);
-        } catch (error) {
+        },
+        error(error) {
           subscriber.error(error);
-        }
-      })();
+        },
+      });
     });
   }
 }
