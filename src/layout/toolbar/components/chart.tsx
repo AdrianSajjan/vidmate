e905ca030144966b46b0a8cb7f44c0630b03b71f
debojitsaha <saha05debojit@gmail.com>
2024-08-01 12:51:59 +0530

    add: point radius

--- src/layout/toolbar/components/chart.tsx
+++ src/layout/toolbar/components/chart.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { useEditorContext } from "@/context/editor";
 import { ChartTypeRegistry } from "chart.js";
-import { Radius } from "lucide-react";
+import { Radius, Waypoints } from "lucide-react";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Label } from "@/components/ui/label";
 import { Slider } from "@/components/ui/slider";
@@ -19,7 +19,6 @@ const changeChartTypeOptions = [
 function _ChartToolbar() {
   const editor = useEditorContext();
   const selected = editor.canvas.selection.active as fabric.Chart;
-  console.log("Selected= ", selected.chart.options?.elements?.bar?.borderRadius);
 
   return (
     <div className="px-1 flex items-center gap-2 h-full w-full overflow-x-scroll scrollbar-hidden">
@@ -48,19 +47,38 @@ function _ChartToolbar() {
       </Select>
       <Popover>
         <PopoverTrigger asChild>
-          <Button size="icon" variant="outline" aria-label="letter-spacing" className="data-[state=open]:bg-card">
+          <Button size="icon" variant="outline" aria-label="border-radius" className="data-[state=open]:bg-card">
             <Radius size={15} />
           </Button>
         </PopoverTrigger>
         <PopoverContent onOpenAutoFocus={(event) => event.preventDefault()} className="pt-2 pb-3 px-3" align="center">
           <Label className="text-xs font-medium">Border Radius</Label>
           <div className="flex items-center justify-between gap-4">
-            <Slider min={2} max={100} value={[selected.chart.options?.elements?.bar?.borderRadius as number]} onValueChange={([radius]) => editor.canvas.chart.changeActiveChartBorderRadius(radius)} />
+            <Slider min={0} max={100} value={[selected.chart.options?.elements?.bar?.borderRadius as number]} onValueChange={([radius]) => editor.canvas.chart.changeActiveChartBorderRadius(radius)} />
             <Input
               type="number"
               className="h-8 w-16 text-xs"
               value={selected.chart.options?.elements?.bar?.borderRadius as number}
               onChange={(event) => editor.canvas.chart.changeActiveChartBorderRadius(Number(event.target.value))}
+            />
+          </div>
+        </PopoverContent>
+      </Popover>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button disabled={selected.chart.type !== "line"} size="icon" variant="outline" aria-label="point-radius" className="data-[state=open]:bg-card">
+            <Waypoints size={15} />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent onOpenAutoFocus={(event) => event.preventDefault()} className="pt-2 pb-3 px-3" align="center">
+          <Label className="text-xs font-medium">Point Radius</Label>
+          <div className="flex items-center justify-between gap-4">
+            <Slider min={2} max={100} value={[selected.chart.options?.elements?.point?.radius as number]} onValueChange={([radius]) => editor.canvas.chart.changeActiveChartPointRadius(radius)} />
+            <Input
+              type="number"
+              className="h-8 w-16 text-xs"
+              value={selected.chart.options?.elements?.point?.radius as number}
+              onChange={(event) => editor.canvas.chart.changeActiveChartPointRadius(Number(event.target.value))}
             />
           </div>
         </PopoverContent>
